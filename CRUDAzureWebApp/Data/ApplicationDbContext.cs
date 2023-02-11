1ef25c4c71c34590cdcc39ddb42ede135917135b
Chiemela <Chiemela242@gmail.com>
2023-02-11 12:50:04 +0000

    Entity Framework Integration

---
+++ CRUDAzureWebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CRUDAzureWebApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUDAzureWebApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
