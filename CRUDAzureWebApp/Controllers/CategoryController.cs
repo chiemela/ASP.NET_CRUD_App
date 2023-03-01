4c161d4cb42f7b12b1d701c6c89bd86b83c96745
Chiemela <Chiemela242@gmail.com>
2023-03-01 05:50:12 +0000

    can now Create and Read data

--- CRUDAzureWebApp/Controllers/CategoryController.cs
+++ CRUDAzureWebApp/Controllers/CategoryController.cs
@@ -1,12 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CRUDAzureWebApp.Data;
+using CRUDAzureWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CRUDAzureWebApp.Controllers
 {
+
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<Category> obbjCategoryList = _db.Categories;
+            return View(obbjCategoryList);
+        }
+
+        // GET
+        public IActionResult Create()
+        {
             return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+
+        }
+
+        // GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFormDB = _db.Categories.Find(id);
+            return View();
+        }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("Name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+
+            return View();
+
         }
     }
 }
